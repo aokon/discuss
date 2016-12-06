f14d90780b831a1aa456b34de3367068b8b0abe4
Adam Okoń <adam.okon@elegantcode.pl>
2016-12-06 14:46:48 +0100

    as an user I can delete a topic

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -29,14 +29,14 @@ defmodule Discuss.TopicController do
   end
 
   def edit(conn, %{"id" => topic_id}) do
-    topic = Repo.get(Topic, topic_id)
+    topic = Repo.get!(Topic, topic_id)
     changeset = Topic.changeset(topic)
 
     render conn, "edit.html", changeset: changeset, topic: topic
   end
 
   def update(conn, %{"id" => topic_id, "topic" => topic_params}) do
-    topic = Repo.get(Topic, topic_id)
+    topic = Repo.get!(Topic, topic_id)
     changeset = Topic.changeset(topic, topic_params)
 
     case Repo.update(changeset) do
@@ -47,5 +47,13 @@ defmodule Discuss.TopicController do
       {:error, changeset} ->
         render conn, "edit.html", changeset: changeset, topic: topic
     end
+  end
+
+  def delete(conn, %{"id" => topic_id}) do
+    Repo.get!(Topic, topic_id) |> Repo.delete!
+
+    conn
+    |> put_flash(:info, "Topic was removed properly.")
+    |> redirect(to: topic_path(conn, :index))
   end
 end
