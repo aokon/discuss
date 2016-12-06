918cf285b4464ea9f52021980c9027c4d5d8b489
Adam Okoń <adam.okon@elegantcode.pl>
2016-12-06 14:34:02 +0100

    as an user I can edit a topic

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -19,12 +19,33 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset(%Topic{}, topic_params)
 
     case Repo.insert(changeset) do
-      {:ok, topic} ->
+      {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic was added properly")
         |> redirect(to: topic_path(conn, :index))
       {:error, changeset} ->
         render conn, "new.html", changeset: changeset
+    end
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic_params}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic, topic_params)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic was updated properly.")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "edit.html", changeset: changeset, topic: topic
     end
   end
 end
